5c490652b70beafe092fb6da03796b3a32c3a813
msturmann <marcelosturmann@googlemail.com>
2022-05-22 20:03:40 -0300

    Modificando el dashboard para que funcione con la API via react classes

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,74 +1,57 @@
-import React from 'react';
-import { useEffect, useState } from 'react/cjs/react.production.min';
+import React, { Component } from 'react';
 import SmallCard from './SmallCard';
 
-
-
-function ContentRowTop(){
-    let [productArray, setProductArray] = useState("Cargando...")
-    let [userArray, setUserArray] = useState("Cargando...")
-    let [compatibilityArray, setCompatibilityArray] = useState("Cargando...")
-
-
-    let productInDataBase = {
-        color:   "primary",
-        titulo: "Total de productos a la venta",
-        valor: productArray.meta.count,
-        icono: "fa-solid fa-boxes-stacked",
+class ContentRowMovies extends Component {
+    constructor() {
+        super()
+        this.state = {
+            cajas: []
+        }
     }
-    
-    let amount ={
-        color:   "success",
-        titulo: "Total de  usuarios",
-        valor: userArray.meta.count,
-        icono: "fas fa-user",
-    }
-    
-    let user = {
-        color:   "warning",
-        titulo: "Total de categorías",
-        valor: compatibilityArray,
-        icono: "fa-solid fa-arrow-down-left-and-arrow-up-right-to-center",
-    }
-    
-    let cardProps = [productInDataBase,amount,user];
 
+    componentDidMount() { 
+        Promise.all([
+            fetch('/api/products')
+            .then(res => res.json()),
 
-    
-useEffect(() => {
-        fetch('/api/products')
-            .then(respuesta => {
-                return respuesta.json()
-            })
-            .then(products => {
-        
-                setProductArray(products)
-            })
-            .catch(error => console.log(error))
             fetch('/api/users')
-            .then(respuesta => {
-                return respuesta.json()
-            })
-            .then(users => {
-        
-                setUserArray(users)
-            })
-            .catch(error => console.log(error))      
-           let quantity= Object.keys(productArray.countByCategory).length
-            setCompatibilityArray(quantity)
-        })
-
-    return (
-        <React.Fragment>
-        {/*<!-- Content Row -->*/}
-        <div className="row">
-            {
-                cardProps.map((producto,index)=>{
-                    return <SmallCard  {...producto}  key= {index}/>
-                })
-            }      
-        </div>
-        </React.Fragment>
-    )
+            .then(res => res.json())])
+            
+            .then((apiData) => { 
+                let cajaProductos = { 
+                    color: "primary",
+                    titulo: "Productos en Base de Datos", 
+                    valor: apiData[0].meta.count,
+                    icono: "fas fa-box", 
+                }; 
+                let cajaUsuarios = { 
+                    color: "success", 
+                    titulo: "usuarios registrados", 
+                    valor: apiData[1].meta.count, 
+                    icono: "fas fa-user",
+                }; 
+                let cajaCompatibilidad = {
+                    color: "warning",
+                    titulo: "Categorias totales",
+                    valor: Object.keys(apiData[0].countByCategory).length,
+                    icono: "fas fa-bookmark",
+                }; 
+            this.setState({ 
+                cajas: [cajaProductos, cajaUsuarios, cajaCompatibilidad] 
+            }); 
+        })}
+        render(){
+            return (
+                <React.Fragment>
+                {/*<!-- Content Row -->*/}
+                    <div className="row">
+                {
+                    this.state.cajas.map((caja,index)=>{
+                        return <SmallCard  {...caja}  key= {index}/>
+                    })
+                }      
+            </div>
+                </React.Fragment>
+        )}
 }
-export default ContentRowTop;
+export default ContentRowMovies;
